9f07ba74a76c2edb5896bf634be4d7075940ff79
davidps79 <1006352045@u.icesi.edu.co>
2022-05-22 21:23:19 -0500

    Folders restructured

---
+++ Test/dataStructures/MatrixTest.java
@@ -0,0 +1,103 @@
+package dataStructures;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MatrixTest {
+	
+	MatrixGraph test;
+	
+	public void setupStage1() {
+		test= new MatrixGraph(true);
+	}
+	
+	public void setupStage2() {
+		test= new MatrixGraph(true);
+		for (int i = 0; i < 5; i++) {
+			test.addVertex();
+		}
+		test.addEdge(0, 1, 10);
+		test.addEdge(2, 4, 5);
+		test.addEdge(0, 3, 12);
+		test.addEdge(3, 1, 1);
+	}
+	
+	public void setupStage3() {
+		test= new MatrixGraph(false);
+		for (int i = 0; i < 5; i++) {
+			test.addVertex();
+		}
+		test.addEdge(0, 1, 10);
+		test.addEdge(2, 4, 5);
+		test.addEdge(0, 3, 12);
+		test.addEdge(3, 1, 1);
+	}
+	
+	
+	@Test 
+	void addVertexTest() {
+		setupStage1();
+		int vertex=6;
+		for (int i = 0; i < vertex; i++) {
+			test.addVertex();
+		}
+		assertEquals(vertex,test.getVertexAmount());
+		
+	}
+	
+	@Test 
+	void addEdgeDirectedGraphTest() {
+		setupStage2();
+		assertEquals(10,test.getAdy()[0][1]);
+		assertEquals(0,test.getAdy()[1][0]);
+		
+		assertEquals(5,test.getAdy()[2][4]);
+		assertEquals(0,test.getAdy()[4][2]);
+		
+		assertEquals(12,test.getAdy()[0][3]);
+		assertEquals(0,test.getAdy()[3][0]);
+		
+		assertEquals(1,test.getAdy()[3][1]);
+		assertEquals(0,test.getAdy()[1][3]);
+		
+	}
+	
+	@Test 
+	void addEdgeNotDirectedGraphTest() {
+		setupStage3();
+		assertEquals(10,test.getAdy()[0][1]);
+		assertEquals(10,test.getAdy()[1][0]);
+		
+		assertEquals(5,test.getAdy()[2][4]);
+		assertEquals(5,test.getAdy()[4][2]);
+		
+		assertEquals(12,test.getAdy()[0][3]);
+		assertEquals(12,test.getAdy()[3][0]);
+		
+		assertEquals(1,test.getAdy()[3][1]);
+		assertEquals(1,test.getAdy()[1][3]);
+		
+	}
+	
+	@Test
+	void removeEdgeDirectedGraphTest() {
+		setupStage2();
+		test.removeEdge(0, 3);
+		
+		assertEquals(0,test.getAdy()[0][3]);
+		assertEquals(0,test.getAdy()[3][0]);
+		
+	}
+	
+	@Test
+	void removeEdgeNotDirectedGraphTest() {
+		setupStage3();
+		test.removeEdge(1, 3);
+		
+		assertEquals(0,test.getAdy()[3][1]);
+		assertEquals(0,test.getAdy()[1][3]);
+		
+	}
+
+
+}
---
+++ src/dataStructures/MatrixGraph.java
@@ -0,0 +1,96 @@
+package dataStructures;
+public class MatrixGraph implements IGraph {
+    private boolean isDirected;
+    private int vertexAmount;
+    private int edgeAmount;
+    private int VERTEX_LIMIT = 100;
+    private int ady[][];
+
+    public boolean isDirected() {
+		return this.isDirected;
+	}
+
+	public void setIsDirected(boolean isDirected) {
+		this.isDirected = isDirected;
+	}
+
+	public int getVertexAmount() {
+		return this.vertexAmount;
+	}
+
+	public void setVertexAmount(int vertexAmount) {
+		this.vertexAmount = vertexAmount;
+	}
+
+	public int getEdgeAmount() {
+		return this.edgeAmount;
+	}
+
+	public void setEdgeAmount(int edgeAmount) {
+		this.edgeAmount = edgeAmount;
+	}
+	
+	public int[][] getAdy() {
+		return ady;
+	}
+
+    public MatrixGraph(boolean isDirected) {
+        this.isDirected = isDirected;
+        this.vertexAmount = 0;
+        this.edgeAmount = 0;
+        this.ady = new int[VERTEX_LIMIT][VERTEX_LIMIT];
+    }  
+    
+    @Override
+    public void addVertex() {
+        vertexAmount++;
+    }
+
+    @Override
+    public void removeVertex(int vertex) {
+        for (int i=0; i<vertexAmount; i++) {
+            ady[i][vertex] = 0;
+        }
+
+        for (int i=0; i<vertexAmount-1-vertex; i++) {
+            for (int j=0; j<vertexAmount; j++) {
+                //System.out.println(i + "--> " + j);
+                ady[j][vertex+i] = ady[j][vertex+i+1];
+                ady[vertex+i][j] = ady[vertex+i+1][j];
+            }
+        }
+
+        for (int i=0; i<vertexAmount-1; i++) {
+            ady[vertexAmount-1][i] = 0;
+            ady[i][vertexAmount-1] = 0;
+        }
+
+        vertexAmount--;
+    }
+
+    @Override
+    public void addEdge(int from, int to, int weight) {
+        ady[from][to] = weight;
+        if (isDirected==false) ady[to][from] = weight;
+        edgeAmount++;
+    }
+
+    @Override
+    public void removeEdge(int from, int to) {
+        ady[from][to] = 0;
+        if (isDirected==false) ady[to][from] = 0;
+        edgeAmount--;
+    }
+
+    @Override
+    public String printAdy(){
+        String s= "";
+        for (int i = 0; i < vertexAmount; i++) {
+            for (int j = 0; j < vertexAmount; j++) {
+                s+= ady[i][j]+" ";
+            }
+            s+="\n";
+        }
+      return s;
+    }
+}
