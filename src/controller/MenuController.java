fb2700261f959f688705471b4d27ac0820dd00be
davidps79 <1006352045@u.icesi.edu.co>
2022-06-01 11:39:08 -0500

    Points and edges with canvas

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.util.ArrayList;
-
 import aplication.Main;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -18,9 +17,8 @@ public class MenuController {
 
 	private ArrayList<String> stationList;
 	private ArrayList<String> implementationList;
-	private int radius = 9;
-	private int radius2 = 2;
-	
+	private int[] mainStations = {0, 1, 10, 16, 17, 18, 48};
+
 	@FXML
 	private Canvas canvasMarks;
 	
@@ -126,19 +124,46 @@ public class MenuController {
 
 	public void drawPoint(int stationIndex) {
 		Station station = main.getBack().getStation(stationIndex);
+		System.out.println(station.getName());
+		float x1 = station.getX();
+		float y1 = station.getY();
 		
-		float x1 = station.getX() + radius;
-		float y1 = station.getY() + radius;
+		int radius = 3;
 		
-		gc.fillOval(x1, y1, radius2, radius2);
+		for (int i=0; i<mainStations.length; i++) {
+			if (stationIndex == mainStations[i]) {
+				radius = 8;
+				break;
+			}
+		}
+		
+		gc.fillOval(x1, y1, radius*2, radius*2);
 	}
 	
 	public void drawEdge(int from, int to) {
 		Station fromStation = main.getBack().getStation(from);
 		Station toStation = main.getBack().getStation(to);
 		
+		int radius = 3;
+		
+		for (int i=0; i<mainStations.length; i++) {
+			if (from == mainStations[i]) {
+				radius = 8;
+				break;
+			}
+		}
+		
 		float x1 = fromStation.getX() + radius;
 		float y1 = fromStation.getY() + radius;
+		
+		radius = 3;
+		
+		for (int i=0; i<mainStations.length; i++) {
+			if (to == mainStations[i]) {
+				radius = 8;
+				break;
+			}
+		}
 		
 		float x2 = toStation.getX() + radius;
 		float y2 = toStation.getY() + radius;
@@ -146,9 +171,19 @@ public class MenuController {
 		gc.strokeLine(x1, y1, x2, y2);
 	}
 	
+	public void drawTravel(int from, int to) {
+		drawPoint(from);
+		drawEdge(from, to);
+		drawPoint(to);
+	}
+	
 	public void setMain(Main main) {
 		this.main = main;
 		gc = canvasMarks.getGraphicsContext2D();
+		drawTravel(0, 4);
+		drawTravel(4, 5);
+		drawTravel(5, 14);
+		drawTravel(14, 23);
 	}
 }
 
--- src/model/TransportationSystem.java
+++ src/model/TransportationSystem.java
@@ -1,7 +1,34 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TransportationSystem {
+	private final int stationNumber = 59;
 	private Station[] stations;
+	
+	public TransportationSystem() {
+		stations = new Station[stationNumber];
+		loadData();
+	}
+	
+	public void loadData() {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(new File("./data/data.csv")));
+			in.readLine();
+			for (int i=0; i<stationNumber; i++) {
+				String[] line = in.readLine().split(",");
+				stations[i] = new Station(line[0], Float.parseFloat(line[2]), Float.parseFloat(line[3]));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public Station getStation(int index) {
 		return stations[index];
